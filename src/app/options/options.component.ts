3d142db1d86f566e3ffa1ba0facdb56c881da1b5
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-13 22:15:42 -0600

    feat: Add new task form dialog to options component

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { TaskformComponent } from '../taskform/taskform.component';
+import { DialogModule } from 'primeng/dialog';
+
 
 @Component({
   selector: 'app-options',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [
+    ButtonModule,
+    TaskformComponent,
+    DialogModule],
   templateUrl: './options.component.html',
   styleUrl: './options.component.scss'
 })
 export class OptionsComponent {
-
+  viewModal = false
+  handlerViewModal() {
+    this.viewModal = true;
+  }
 }
