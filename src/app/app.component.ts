a73bcbd19846485a15f903b37a4a71b512e8d2f5
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-14 20:59:31 -0600

    chore: Add tab menu and today tasks component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { OptionsComponent } from './options/options.component';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TodaytasksComponent } from './todaytasks/todaytasks.component';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,21 @@ import { OptionsComponent } from './options/options.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    OptionsComponent
+    OptionsComponent,
+    TabMenuModule,
+    TodaytasksComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  selectedType: 'Overdue' | 'Upcoming' | 'Completed' = 'Overdue'
+
+  items = [
+    { label: 'Overdue ', icon: 'pi pi-calendar-times', command: () => { this.selectedType = "Overdue" } },
+    { label: 'Upcoming ', icon: 'pi pi pi-hourglass', command: () => this.selectedType = "Upcoming" },
+    { label: 'Completed ', icon: 'pi pi-check-square', command: () => this.selectedType = "Completed" },
+  ]
 
 }
 
