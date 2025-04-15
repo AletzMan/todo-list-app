9ce9338485df533e2987315443b28b9c49bf07f8
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-15 00:51:12 -0600

    chore: Update app component to include search page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { OptionsComponent } from './options/options.component';
 import { TodaytasksComponent } from './todaytasks/todaytasks.component';
 import { CalendarpageComponent } from './calendarpage/calendarpage.component';
+import { SearchpageComponent } from './searchpage/searchpage.component';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ import { CalendarpageComponent } from './calendarpage/calendarpage.component';
     HeaderComponent,
     OptionsComponent,
     /*TodaytasksComponent*/
-    CalendarpageComponent
+    /*CalendarpageComponent*/
+    SearchpageComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
