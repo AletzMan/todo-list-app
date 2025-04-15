0007fa90d2a8a2984e3fadf47028266b054f10ba
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-14 23:52:21 -0600

    chore: Add calendar page component and update app component to include it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { OptionsComponent } from './options/options.component';
 import { TodaytasksComponent } from './todaytasks/todaytasks.component';
+import { CalendarpageComponent } from './calendarpage/calendarpage.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { TodaytasksComponent } from './todaytasks/todaytasks.component';
     HeaderComponent,
     OptionsComponent,
     /*TodaytasksComponent*/
+    CalendarpageComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
