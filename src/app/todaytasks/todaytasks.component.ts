addf1c637204d4e05647799c59c254bb665b9471
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-14 23:14:09 -0600

    chore: Update todaytasks component to include tab menu for task type selection

--- src/app/todaytasks/todaytasks.component.ts
+++ src/app/todaytasks/todaytasks.component.ts
@@ -3,6 +3,7 @@ import { ProgressBarModule } from 'primeng/progressbar';
 import { Categories } from '../../constants';
 import { CheckboxModule } from 'primeng/checkbox';
 import { TaskviewComponent } from '../taskview/taskview.component';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 export interface Task {
   name: string
@@ -20,13 +21,22 @@ type Categories = 'work' | 'sport' | 'study' | 'social' | 'home' | 'shopping' | 
   imports: [
     ProgressBarModule,
     CheckboxModule,
-    TaskviewComponent
+    TaskviewComponent,
+    TabMenuModule
   ],
   templateUrl: './todaytasks.component.html',
   styleUrl: './todaytasks.component.scss'
 })
 export class TodaytasksComponent {
-  @Input() taskType: 'Overdue' | 'Upcoming' | 'Completed' = "Overdue";
+
+  selectedType: 'Overdue' | 'Upcoming' | 'Completed' = 'Overdue'
+
+  items = [
+    { label: 'Overdue ', icon: 'pi pi-calendar-times', command: () => { this.selectedType = "Overdue" } },
+    { label: 'Upcoming ', icon: 'pi pi pi-hourglass', command: () => this.selectedType = "Upcoming" },
+    { label: 'Completed ', icon: 'pi pi-check-square', command: () => this.selectedType = "Completed" },
+  ]
+
 
   tasks: Task[] = [
     {
