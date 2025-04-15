2d57ca846a5e552ddb368b9884b49f2ae4800290
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-14 23:14:03 -0600

    chore: Remove unused code and comments in app.component.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { OptionsComponent } from './options/options.component';
-import { TabMenuModule } from 'primeng/tabmenu';
 import { TodaytasksComponent } from './todaytasks/todaytasks.component';
 
 @Component({
@@ -12,20 +11,12 @@ import { TodaytasksComponent } from './todaytasks/todaytasks.component';
     RouterOutlet,
     HeaderComponent,
     OptionsComponent,
-    TabMenuModule,
-    TodaytasksComponent
+    /*TodaytasksComponent*/
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  selectedType: 'Overdue' | 'Upcoming' | 'Completed' = 'Overdue'
-
-  items = [
-    { label: 'Overdue ', icon: 'pi pi-calendar-times', command: () => { this.selectedType = "Overdue" } },
-    { label: 'Upcoming ', icon: 'pi pi pi-hourglass', command: () => this.selectedType = "Upcoming" },
-    { label: 'Completed ', icon: 'pi pi-check-square', command: () => this.selectedType = "Completed" },
-  ]
 
 }
 
