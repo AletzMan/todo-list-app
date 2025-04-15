d870ae6a12040438c0adbd98a1773db940692f5f
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-15 17:05:19 -0600

    feat: Add search functionality to search page component

--- src/app/searchpage/searchpage.component.ts
+++ src/app/searchpage/searchpage.component.ts
@@ -3,6 +3,12 @@ import { ButtonModule } from 'primeng/button';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
+import { Task } from '../todaytasks/todaytasks.component';
+import { TasksArray } from '../../utils/constants';
+import { TaskviewComponent } from '../taskview/taskview.component';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-searchpage',
@@ -11,11 +17,30 @@ import { InputTextModule } from 'primeng/inputtext';
     InputTextModule,
     InputGroupModule,
     ButtonModule,
-    InputGroupAddonModule
+    InputGroupAddonModule,
+    TaskviewComponent,
+    FormsModule, ReactiveFormsModule,
+    DropdownModule,
+    CardModule
   ],
   templateUrl: './searchpage.component.html',
   styleUrl: './searchpage.component.scss'
 })
 export class SearchpageComponent {
+
+  formSearch: FormGroup = new FormGroup({
+    search: new FormControl<string>("")
+  })
+
+  tasks: Task[] = TasksArray
+  filterTasks: Task[] = []
+
+  onSearch() {
+    const wordSearch = this.formSearch.value.search;
+    if (wordSearch !== "") {
+      this.filterTasks = this.tasks.filter(newTasks => newTasks.name.toLowerCase().includes(wordSearch.toLowerCase()))
+    }
+    console.log(this.filterTasks)
+  }
 
 }
