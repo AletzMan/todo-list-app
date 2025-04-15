9e6327bf9287e67d0df60e02fd464c74972cc544
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-14 20:59:41 -0600

    chore: Add user name and avatar to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  user = {
+    name: "Alejandro",
+    avatar: "https://media.licdn.com/dms/image/v2/D4E03AQGXowISWQp08w/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1706248074328?e=1750291200&v=beta&t=XQ7VVQ4RNJcar9nga_TmiTBkFZNKb0sP8MamDYxMqMM"
+  }
 }
