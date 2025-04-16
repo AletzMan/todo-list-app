33dae70e41c0d49920a52270b5516d999517d2a5
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-16 13:20:56 -0600

    chore: Update app routes to include new pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { TodaytasksComponent } from './todaytasks/todaytasks.component';
+import { CalendarpageComponent } from './calendarpage/calendarpage.component';
+import { SearchpageComponent } from './searchpage/searchpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: TodaytasksComponent
+    },
+    {
+        path: "calendar",
+        component: CalendarpageComponent
+    },
+    {
+        path: "search",
+        component: SearchpageComponent
+    }
+];
