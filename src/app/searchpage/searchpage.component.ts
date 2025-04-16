50c90d3778d0ed038dfa2354450ec7883b1753d4
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-15 19:37:42 -0600

    chore: Update search page component with improved search functionality and filters

--- src/app/searchpage/searchpage.component.ts
+++ src/app/searchpage/searchpage.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
 import { Task } from '../todaytasks/todaytasks.component';
-import { TasksArray } from '../../utils/constants';
+import { CategoriesArray, StatusArray, TasksArray } from '../../utils/constants';
 import { TaskviewComponent } from '../taskview/taskview.component';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
@@ -26,21 +26,74 @@ import { CardModule } from 'primeng/card';
   templateUrl: './searchpage.component.html',
   styleUrl: './searchpage.component.scss'
 })
-export class SearchpageComponent {
+export class SearchpageComponent implements OnInit {
+
+  categories = CategoriesArray;
+  status = StatusArray;
+
 
   formSearch: FormGroup = new FormGroup({
-    search: new FormControl<string>("")
+    search: new FormControl<string>(""),
+    currentCategory: new FormControl<string | undefined>(undefined),
+    currentStatus: new FormControl<string | undefined>(undefined)
   })
 
   tasks: Task[] = TasksArray
   filterTasks: Task[] = []
 
+  ngOnInit() {
+    this.filterTasks = this.tasks;
+  }
+
   onSearch() {
+    this.updateFilters();
+  }
+
+  onChangeCategory() {
+    this.updateFilters();
+  }
+
+  onChangeStatus() {
+    this.updateFilters();
+  }
+
+  private updateFilters() {
     const wordSearch = this.formSearch.value.search;
+    const selectedStatus = this.formSearch.value.currentStatus?.key; // Usar encadenamiento opcional
+    const selectedCategory = this.formSearch.value.currentCategory?.key; // Usar encadenamiento opcional
+
+    // Empezar con la lista completa de tareas
+    this.filterTasks = [...this.tasks];
+
+    // Aplicar filtro por búsqueda de palabra clave
     if (wordSearch !== "") {
-      this.filterTasks = this.tasks.filter(newTasks => newTasks.name.toLowerCase().includes(wordSearch.toLowerCase()))
+      this.filterTasks = this.filterTasks.filter(task =>
+        task.name.toLowerCase().includes(wordSearch.toLowerCase())
+      );
     }
-    console.log(this.filterTasks)
+
+    // Aplicar filtro por estado
+    if (selectedStatus) {
+      if (selectedStatus === "all") {
+        this.filterTasks = this.filterTasks
+      } else {
+
+        this.filterTasks = this.filterTasks.filter(task =>
+          task.status.toLowerCase() === selectedStatus.toLowerCase()
+        );
+      }
+    }
+
+    // Aplicar filtro por categoría
+    if (selectedCategory) {
+      if (selectedCategory === "all") {
+        this.filterTasks = this.filterTasks
+      } else {
+        this.filterTasks = this.filterTasks.filter(task =>
+          task.category.toLowerCase() === selectedCategory.toLowerCase()
+        );
+      }
+    }
   }
 
 }
