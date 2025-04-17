d759ed5962a3d25de611effd029e491f4ddcda50
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-17 01:09:19 -0600

    feat: Add lazy loading for images in todaytasks-page component

--- src/app/pages/todaytasks-page/todaytasks-page.component.ts
+++ src/app/pages/todaytasks-page/todaytasks-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { Categories, TasksArray } from '../../../utils/constants';
 import { CheckboxModule } from 'primeng/checkbox';
@@ -28,17 +28,22 @@ type Categories = 'work' | 'sport' | 'study' | 'social' | 'home' | 'shopping' | 
   templateUrl: './todaytasks-page.component.html',
   styleUrl: './todaytasks-page.component.scss'
 })
-export class TodaytasksPageComponent {
+export class TodaytasksPageComponent implements OnInit {
 
   selectedType: 'Overdue' | 'Upcoming' | 'Completed' = 'Overdue'
 
   items = [
-    { label: 'Overdue ', icon: 'pi pi-calendar-times', command: () => { this.selectedType = "Overdue" } },
-    { label: 'Upcoming ', icon: 'pi pi pi-hourglass', command: () => this.selectedType = "Upcoming" },
-    { label: 'Completed ', icon: 'pi pi-check-square', command: () => this.selectedType = "Completed" },
+    { label: 'Overdue ', icon: 'pi pi-calendar-times', command: () => { this.selectedType = "Overdue", this.tasks = TasksArray.filter(task => task.status === "Overdue") } },
+    { label: 'Upcoming ', icon: 'pi pi pi-hourglass', command: () => { this.selectedType = "Upcoming", this.tasks = TasksArray.filter(task => task.status === "Upcoming") } },
+    { label: 'Completed ', icon: 'pi pi-check-square', command: () => { this.selectedType = "Completed", this.tasks = TasksArray.filter(task => task.status === "Completed") } },
   ]
 
 
-  tasks: Task[] = TasksArray
+  tasks: Task[] = []
+
+
+  ngOnInit() {
+    this.tasks = TasksArray.filter(task => task.status === "Overdue")
+  }
 
 }
