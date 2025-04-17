b18f2426caa0ea8a8d7cf4661b9f9a843333bbe1
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-17 00:03:24 -0600

    chore: Remove commented out code in app component and update app options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { OptionsComponent } from './options/options.component';
-import { TodaytasksComponent } from './todaytasks/todaytasks.component';
-import { CalendarpageComponent } from './calendarpage/calendarpage.component';
-import { SearchpageComponent } from './searchpage/searchpage.component';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +9,7 @@ import { SearchpageComponent } from './searchpage/searchpage.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    OptionsComponent,
-    /*TodaytasksComponent*/
-    /*CalendarpageComponent*/
+    OptionsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
